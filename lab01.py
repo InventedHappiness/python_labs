ea14caddcbe1f108f7d538c3672317c9e292b8b1
InventedHappiness <mishahvorostenko@gmail.com>
2026-09-25 14:51:04 +0300

    Uncommented code for various input checks

--- lab01.py
+++ lab01.py
@@ -1,6 +1,37 @@
 # #1
-# a = int(input())
+# a = float(input("Введіть число а"))
 # if a % 2 == 0:
 #     print("parne")
 # else:
 #     print("ne parne")
+
+# age = float(input("Введіть скільки вам років"))
+# if age >= 18:
+#      print("Ви повнолітні!")
+# else:
+#     print("Ви неповнолітні!")
+
+# import math
+# radius = input("Введіть радіус")
+# length = int(radius * math.pi * 2)
+# area = int(math.pi *(radius ** 2))
+
+# a = input("Введіть число а")
+# b = input("Введіть число b")
+# if a > b:
+#     print("Число а більше")
+# else:
+#     print("Число b більше")
+
+# #2
+
+# x = float(input("Введіть значення x"))
+# y = float(input("Введіть значення y"))
+# if x > 0 and y > 0:
+#     print("Ваша точка знаходиться в Першій чверті")
+# elif x < 0 and y > 0:
+#     print("Ваша точка знаходиться в Другій чверті")
+# elif x < 0 and y < 0:
+#     print("Ваша точка знаходиться в Третій чверті")
+# else:
+#     print("Ваша точка знаходиться в Четвертій чверті")
